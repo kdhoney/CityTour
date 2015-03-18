123aab1300216c3c1c32cc1706279422378d3147
kdhoney <game2k@naver.com>
2015-03-18 18:23:23 +0900

    메뉴완성 and 구글맵

---
+++ MyCityTourApp/app/src/main/java/com/example/doheon/mycitytourapp/contentActivity.java
@@ -0,0 +1,14 @@
+package com.example.doheon.mycitytourapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by DoHeon on 2015-03-17.
+ */
+public class contentActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- MyCityTourApp/app/src/main/java/com/example/doheon/mycitytourapp/menuActivity.java
+++ MyCityTourApp/app/src/main/java/com/example/doheon/mycitytourapp/menuActivity.java
@@ -1,17 +1,37 @@
 package com.example.doheon.mycitytourapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by DoHeon on 2015-03-16.
  */
-public class menuActivity extends Activity
-{
+public class menuActivity extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void onClick(View view) {
+
+        Intent intent = new Intent(this, MapsActivity.class);
+        switch(view.getId())
+        {
+            case R.id.btn_1:
+                startActivity(intent);
+                break;
+            case R.id.btn_2:
+                startActivity(intent);
+                break;
+            case R.id.btn_3:
+                startActivity(intent);
+                break;
+            case R.id.btn_4:
+                startActivity(intent);
+                break;
+        }
     }
 }
